1b1d289149e58808149def2c98102285af8b27f1
Nathan Kewley <nathan.kewley@gmail.com>
2021-04-07 16:10:07 +1000

    generate az group deploy command from config and bicep

--- lib/deploy.py
+++ lib/deploy.py
@@ -4,21 +4,24 @@ def load_config(config):
     with open(f"configuration/{config}") as file:
         return yaml.load(file, Loader=yaml.FullLoader)
 
-# def create_resource_group(rg):
-#     return 0
+def build_param_string(params):
+    param_string = ""
+    for param, value in params.items():
+        param_string = param_string + f"{param}={value} "
+    return param_string[:-1]
 
-def build_bicep_deployment(configuration, bicep):
-    # need to validate and generate a final bicep template here
-    return 0
-
-def deploy_bicep(configuration, bicep):
-    build_bicep_deployment(configuration, bicep)
-    # os.deploy(az cli here)
+def deploy_bicep(params, bicep, resource_group):
+    parameters = build_param_string(params)
+    azure_cli_command = f"az deployment group create -f bicep/{bicep} -g {resource_group} --mode Complete --parameters {parameters}"
+    print(azure_cli_command)
 
 def deploy(configuration):
-    print(f"Deploying: {configuration}")
     config = load_config(configuration)
-    print(config)
+    print(f"Deploying: {configuration}")
+    deploy_bicep(config['params'], config['bicep_path'], configuration.split('/')[1])
+
+# def create_resource_group(rg):
+#     return 0
 
 # def deployRg(rg):
 #     return 0
