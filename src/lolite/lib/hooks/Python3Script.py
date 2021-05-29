8e8ae64dd66bb55bb794b05b343a1bf9d379a78d
NathanKewley <nathan.kewley@gmail.com>
2021-05-29 12:31:23 +1000

    more explicit hook name

--- src/lolite/lib/hooks/Python3Script.py
+++ src/lolite/lib/hooks/Python3Script.py
@@ -8,6 +8,6 @@ class Hook(HookBase):
         super().__init__(logger, arguments)
 
     def execute_hook(self):
-        self._logger.debug(f"Running Python Hook: {self._arguments}")
+        self._logger.debug(f"Running Python3 Hook: {self._arguments}")
         if not self._subproc.run_command_exit_code(f"python3 {self._arguments}") == 0:
             raise Exception(f"Python Hook Retuned non 0 result ({self._arguments})")
